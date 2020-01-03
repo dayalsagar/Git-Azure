3584c08a9992366345fd64aaa818a9843e6ed597
Dayal Sagar <dayalsagar8@gmail.com>
2020-01-03 05:48:24 +0530

    My first Git Program

---
+++ GitAzure/GitAzure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitAzure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
